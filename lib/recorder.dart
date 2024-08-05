46b887fe703c4bdfd2cb14a92b0a52d29d9c88e8
elnazyavari <eleennazii.yavari.cs@gmail.com>
2024-08-05 14:45:07 +0330

    recorder platfrom specific configs

--- lib/recorder.dart
+++ lib/recorder.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:async';
 import 'dart:math';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:record/record.dart';
@@ -40,25 +41,33 @@ class _RecorderState extends State<Recorder> {
     try {
       //state setup
       setState(() => recording = true);
-      final tempDir = await getTemporaryDirectory();
-      final punasTempDir =
-          await Directory('${tempDir.path}/punas_support_temp').create();
-      final vmsgPath = '${punasTempDir.path}/vmsg_'
+      Directory? tempDir;
+      Directory? punasTempDir;
+      if (!kIsWeb) {
+        tempDir = await getTemporaryDirectory();
+        punasTempDir =
+            await Directory('${tempDir.path}/punas_support_temp').create();
+      }
+      final vmsgPath = '${punasTempDir?.path}/vmsg_'
           '${DateTime.now().microsecondsSinceEpoch}.m4a';
       if (await record.hasPermission()) {
         await record.start(
           const RecordConfig(encoder: AudioEncoder.aacLc),
           path: vmsgPath,
         );
-        final ampStream = record.onAmplitudeChanged(
-          const Duration(milliseconds: 1),
-        );
         //wave setup
         recordingWaveCtrl.color = Colors.green;
+
+        final ampStream = record.onAmplitudeChanged(
+          const Duration(milliseconds: 1000),
+        );
         ampSubcription = ampStream.listen((amp) {
-          final temp = max(0.0, 1 - ((amp.current / (amp.max / 4))));
+          final temp = max(
+            kIsWeb ? 0.5 : 0.1,
+            1 - ((amp.current / (amp.max / 4))),
+          );
           recordingWaveCtrl.amplitude = temp;
-          recordingWaveCtrl.speed = max(0.0, temp / 10);
+          recordingWaveCtrl.speed = temp / 10;
         });
       }
     } catch (_) {
@@ -77,7 +86,6 @@ class _RecorderState extends State<Recorder> {
       recordingWaveCtrl.color = Colors.grey.shade900;
       recordingWaveCtrl.amplitude = 0.001;
       recordingWaveCtrl.speed = 0.001;
-
       widget.onDone(recordFile);
     } catch (_) {
       setState(() => recording = true);
